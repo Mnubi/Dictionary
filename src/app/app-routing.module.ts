716538bcde316226c4f0289561f741d15dbf66ca
mnubi <rodger7kamau@gmail.com>
2022-07-12 17:01:27 +0300

    added route to home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DictionaryComponent } from './dictionary/dictionary.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',component:DictionaryComponent},
+  {path: 'Navbar',component:NavbarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
