24eaab7528748962853d335cefb42759b5c8fedf
mnubi <rodger7kamau@gmail.com>
2022-07-12 17:02:09 +0300

    imported component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DictionaryComponent } from './dictionary/dictionary.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DictionaryComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
